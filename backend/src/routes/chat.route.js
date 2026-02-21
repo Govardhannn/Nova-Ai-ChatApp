d925a4fdacb30f43069c4488efd14a4ca87f4fad
Govardhannn <sahanigovardhan69@gmail.com>
2026-02-21 19:38:46 +0000

    feat(backend): implement chat API with mongoose

--- backend/src/routes/chat.route.js
+++ backend/src/routes/chat.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { userMiddleware } from "../middleware/auth.middleware.js";
+import { createChat } from "../controllers/chat.controller.js";
 
 const chatRote = express.Router();
 
-chatRote.post('/',userMiddleware, )
+chatRote.post('/',userMiddleware, createChat)
 
 
 
